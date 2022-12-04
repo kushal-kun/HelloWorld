762b27b5d382f2738a5ea4785727a7f02b4cf2d1
kushal-kun <kushaly666@gmail.com>
2022-12-04 18:26:03 +0530

    Update simultaneous.java

--- simultaneous.java
+++ simultaneous.java
@@ -1,106 +1,78 @@
 import java.util.*;
-class simultaneous{
-    static Scanner sc = new Scanner(System.in);
+public class simultaneous {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        int n,i,j;
-        try{
-            System.out.println("enter size of system");
-            n = sc.nextInt();
-            double x[] = new double[n];
-            double b[] = new double[n];
-            double a[][] = new double[n][n];
-            System.out.println("enter coefficients rowwise");
-            for(i=0;i<n;i++){
-                System.out.println("row:"+(i+1));
-                for(j=0;j<n;j++){
-                    a[i][j]=sc.nextDouble();
-                }
-            }
-            System.out.println("enter vector b");
-            for(j=0;j<n;j++){
-                b[j]=sc.nextDouble();
-            }
-            int index[] = new int[n];
-            x=gaussian(a,b,index);
-            System.out.println("solution vector:");
-            for(i=0;i<n;i++){
-                System.out.println(x[i]+" ");
-            }
-        }catch(Exception e){
-            System.out.println("exception occured");
+        System.out.println("number of variables");
+        int n = sc.nextInt();
+        double matrix[][] = new double[n][n+1];
+        ArrayList<Double> values = new ArrayList<Double>();
+        populate(matrix,n);
+        printmatrix(matrix, n);
+        convert(matrix, n);
+        System.out.println();
+        System.out.println();
+        printmatrix(matrix, n);
+        calculate(matrix, n, values);
+        System.out.println();
+        System.out.println();
+        for(int i=0;i<n;i++){
+            values.set(i, ((double)Math.round(values.get(i))));
         }
+        System.out.println(values);
     }
-    public static double[] gaussian(double a[][],double b[],int index[]){
-        int n = b.length;
-        double x[] = new double[n];
-        gaussian(a,index);
-        for(int i=0;i<n-1;++i){
-            for(int j=i+1;j<n;++j){
-                b[index[j]]-=a[index[j]][i]*b[index[i]];
-            }
-        }
-        x[n-1]=b[index[n-1]]/a[index[n-1]][n-1];
-        for(int i=n-2;i>=0;--i){
-            x[i]=b[index[i]];
-            for(int j=1+i;j<n;++j){
-                x[i]/=a[index[i]][i];
-            }
-        }
-        order(x,index);
-        return x;
-    }
-    public static void gaussian(double a[][],int index[]){
-        int n=index.length;
-        double c[] = new double[n];
-        for(int i=0;i<n;++i){
-            index[i]=i;
-        }
-        for(int i=0;i<n;++i){
-            double c1=0;
-            for(int j=0;j<n;++j){
-                double c0=Math.abs(a[i][j]);
-                if(c0>c1){
-                    c1=c0;
-                }
-                c[i]=c1;
-            }
-        }
-        int k=0;
-        for(int j=0;j<n-1;++j){
-            double pi1 = 0;
-            for(int i=j;i<n;++i){
-                double pi0=Math.abs(a[index[i]][j]);
-                pi0/=c[index[i]];
-                if(pi0>pi1){
-                    pi1=pi0;
-                    k=i;
-                }
-            }
-            int itmp = index[j];
-            index[j]=index[k];
-            index[k]=itmp;
-            for(int i=j+1;i<n;++i){
-                double pj = a[index[i]][j]/a[index[j]][j];
-                a[index[i]][j]=pj;
-                for(int l=j+1;l<n;++l){
-                    a[index[i]][l]-=pj*a[index[j]][l];
-                }
+    public static void populate(double matrix[][],int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=n;j++){
+                matrix[i][j]=sc.nextInt();
             }
         }
     }
-    public static void order(double x[],int index[]){
-        int m=x.length;
-        for(int i=0;i<m;++i){
-            for(int j=i+1;j<m;++j){
-                if(index[i]>index[j]){
-                    int itmp=index[i];
-                    index[i]=index[j];
-                    index[j]=itmp;
-                    double xtmp = x[i];
-                    x[i]=x[j];
-                    x[j]=xtmp;
+    public static void printmatrix(double matrix[][],int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void convert(double matrix[][],int n){
+        int current = 0;
+        for(int i=0;i<n-1;i++){
+            for(int k=i+1;k<n;k++){
+                double t = matrix[k][current];
+                for(int j=current;j<=n;j++){
+                    matrix[k][j]=((double)matrix[k][j])/t;
+                    matrix[k][j]=matrix[k][j]*matrix[i][current];
+                    matrix[k][j]=matrix[k][j]-matrix[i][j];
                 }
             }
+            current++;
+        }
+        current=0;
+        for(int i=0;i<n;i++){
+            double t = matrix[i][current];
+            for (int j=0;j<=n;j++){
+                matrix[i][j]=((double)matrix[i][j])/t;
+            }
+            current++;
+        }
+    }
+    public static void calculate(double matrix[][],int n,ArrayList<Double> values){
+        int current=n-1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(j<=current){
+                    System.out.println("break");
+                    break;
+                }
+                matrix[i][n]-=matrix[i][j];
+                System.out.println(matrix[i][n]);
+            }
+            values.add(0,matrix[i][n]);
+            for(int k=n-1;k>=0;k--){
+                matrix[k][current]*=matrix[i][n];
+            }
+            current--;
         }
     }
 }
